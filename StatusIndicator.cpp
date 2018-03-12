63ce60fc07f8c7f0f8e0a8ff068cb7f6c61bb683
Scott Linenberger <scott.linenberger@gmail.com>
2018-03-11 22:57:50 -0500

    remove deprecated status string

--- StatusIndicator.cpp
+++ StatusIndicator.cpp
@@ -14,48 +14,37 @@ void StatusIndicator::begin(Adafruit_NeoPixel &_neoPixels) {
 }
 
 void StatusIndicator::run() {
-  /* setup the state string */
-  String stateString = "Current status is: ";
-
   switch (state) {
     case STATE_GREEN:
       displayColor(neoPixels->Color(0, 255, 20));
-      currentStatus = stateString + "Green";
       break;
 
     case STATE_YELLOW:
       displayColor(neoPixels->Color(200, 200, 0));
-      currentStatus = stateString + "Yellow";
       break;
 
     case STATE_BLUE:
       displayColor(neoPixels->Color(50, 50, 255));
-      currentStatus = stateString + "Blue";
       break;
 
     case STATE_PURPLE:
       displayColor(neoPixels->Color(255, 0, 255));
-      currentStatus = stateString + "Purple";
       break;
 
     case STATE_RED:
       displayColor(neoPixels->Color(255, 10, 10));
-      currentStatus = stateString + "Red";
       break;
 
     case STATE_GREEN_PULSING:
-      currentStatus = stateString + "Green Pulsing";
       fader.fade(neoPixels, false, true, false);
       break;
 
     case STATE_RED_PULSING:
-      currentStatus = stateString + "Red Pulsing";
       fader.fade(neoPixels, true, false, false);
       break;
 
     case STATE_PARTY_TIME:
       pixelParty.party(neoPixels);
-      currentStatus = stateString + "Party Time";
       break;
 
     default:
@@ -81,7 +70,4 @@ void StatusIndicator::updateState(int _state) {
 
   /* immediately call run to update the current status */
   run();
-
-  /* return the status string */
-  return currentStatus;
 }
